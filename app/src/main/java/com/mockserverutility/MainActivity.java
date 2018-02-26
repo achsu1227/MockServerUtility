7ed050eff45a38274e96716e1f08b79db575de0c
ac.hsu <ac.hsu@104.com.tw>
2018-02-26 14:55:13 +0800

    add library module

--- app/src/main/java/com/mockserverutility/MainActivity.java
+++ app/src/main/java/com/mockserverutility/MainActivity.java
@@ -3,7 +3,7 @@ package com.mockserverutility;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ mockserverlibrary/src/test/java/AllTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by ac on 2017/11/16.
+ */
+
+public class AllTest {
+    @Test
+    public void test_get_verify_code() {
+
+    }
+
+
+}
